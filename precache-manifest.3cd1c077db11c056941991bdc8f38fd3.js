9ea88219f3c95715494ade9235fed6de78176fd9
Patipol Wuthiprasert <abcteapatipol@gmail.com>
2020-07-14 16:01:52 +0700

    Updates

---
+++ precache-manifest.3cd1c077db11c056941991bdc8f38fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25236400398ee9a3530e85adccbdb0f",
+    "url": "/shippop-homepage-clone/index.html"
+  },
+  {
+    "revision": "128cc2c63ac7fc08698d",
+    "url": "/shippop-homepage-clone/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "b66c37c778734f3cfe77",
+    "url": "/shippop-homepage-clone/static/css/main.a4c0006a.chunk.css"
+  },
+  {
+    "revision": "128cc2c63ac7fc08698d",
+    "url": "/shippop-homepage-clone/static/js/2.b241dd4a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shippop-homepage-clone/static/js/2.b241dd4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66c37c778734f3cfe77",
+    "url": "/shippop-homepage-clone/static/js/main.795c54ca.chunk.js"
+  },
+  {
+    "revision": "0855d4206bfd3936d1b7",
+    "url": "/shippop-homepage-clone/static/js/runtime-main.b5b58d37.js"
+  }
+]);
