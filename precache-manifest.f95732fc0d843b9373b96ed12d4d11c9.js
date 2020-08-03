5d3182db4a6448b53fcfc7fac04631a8f0662f85
Patipol Wuthiprasert <abcteapatipol@gmail.com>
2020-08-03 15:17:44 +0700

    Updates

--- precache-manifest.f95732fc0d843b9373b96ed12d4d11c9.js
+++ precache-manifest.f95732fc0d843b9373b96ed12d4d11c9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "87dfff22e4abe61f89e7641d4b33e0cb",
+    "revision": "ae405ac3e8253341dac440a0ddf4573e",
     "url": "/shippop-homepage-clone/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/shippop-homepage-clone/static/css/2.4c4477be.chunk.css"
   },
   {
-    "revision": "d59949601c831957cd37",
+    "revision": "499260c8d3bcb0b3e7a3",
     "url": "/shippop-homepage-clone/static/css/main.4c241d8b.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/shippop-homepage-clone/static/js/2.05adc169.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d59949601c831957cd37",
-    "url": "/shippop-homepage-clone/static/js/main.90bacc29.chunk.js"
+    "revision": "499260c8d3bcb0b3e7a3",
+    "url": "/shippop-homepage-clone/static/js/main.a67df0dd.chunk.js"
   },
   {
     "revision": "038c7b2d25e5b2b42f8b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shippop-homepage-clone/precache-manifest.92e5370d15f0ab486028926d58cce6c5.js"
+  "/shippop-homepage-clone/precache-manifest.f95732fc0d843b9373b96ed12d4d11c9.js"
 );
 
 self.addEventListener('message', (event) => {
